a4849c1dbdfd87f8969fbec59093bed7db2a2568
NuclMe <saltansergei@gmail.com>
2024-07-13 23:53:39 +0300

    made working fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { Typography } from 'antd';
 const { Title } = Typography;
-
+import { CoinsTable } from './CoinsTable';
 const App: React.FC = () => {
   return (
     <>
       <Title style={{ fontSize: '24px' }}>Coins & Markets</Title>
+      <CoinsTable />
     </>
   );
 };
--- src/CoinsTable.tsx
+++ src/CoinsTable.tsx
@@ -1,6 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table } from 'antd';
 
-const CoinsTable: React.FC = () => {
-  return <div>CoinsTable</div>;
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Current Price',
+    dataIndex: 'current_price',
+    key: 'current_price',
+  },
+  {
+    title: 'Circulating Supply',
+    dataIndex: 'circulating_supply',
+    key: 'circulating_supply',
+  },
+];
+export const CoinsTable: React.FC = () => {
+  const [cryptos, setCryptos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCryptos = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+        );
+        const data = await response.json();
+        setCryptos(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCryptos();
+  }, []);
+
+  return <Table dataSource={cryptos} columns={columns} />;
 };
