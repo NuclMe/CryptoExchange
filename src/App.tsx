64c383b1306552bdef7cae157c25ac678e225491
NuclMe <saltansergei@gmail.com>
2024-07-13 18:06:39 +0300

    TASK-1_CreateApp

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Typography } from 'antd';
+const { Title } = Typography;
+
+function App() {
+  return (
+    <>
+      <Title>Coins & Markets</Title>
+    </>
+  );
+}
+
+export default App;
+
