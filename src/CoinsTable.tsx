322344b8562f2aa92f61d4e4ac97b499044d6882
NuclMe <saltansergei@gmail.com>
2024-07-14 18:17:42 +0300

    working pagination

--- src/CoinsTable.tsx
+++ src/CoinsTable.tsx
@@ -26,24 +26,27 @@ export const CoinsTable: React.FC = () => {
   const [cryptos, setCryptos] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+
+  const fetchCryptos = async (page, pageSize) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${pageSize}&page=${page}&sparkline=false`
+      );
+      const data = await response.json();
+      setCryptos(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchCryptos = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(
-          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
-        );
-        const data = await response.json();
-        setCryptos(data);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchCryptos();
-  }, []);
+    fetchCryptos(1, 10);
+  }, [page, pageSize]);
 
   return (
     <Table
@@ -51,10 +54,18 @@ export const CoinsTable: React.FC = () => {
       dataSource={cryptos}
       columns={columns}
       pagination={{
-        defaultPageSize: 10,
         showSizeChanger: true,
-        onChange: () => {
-          return console.log('test');
+        pageSize: pageSize,
+        total: 10000,
+        onChange: (page, pageSize) => {
+          setPageSize(pageSize);
+          setPage(page);
+          fetchCryptos(page, pageSize);
+        },
+        onShowSizeChange: (page, pageSize) => {
+          setPageSize(pageSize);
+          setPage(page);
+          fetchCryptos(page, pageSize);
         },
         pageSizeOptions: ['5', '10', '20', '50', '100'],
       }}
