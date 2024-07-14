6fa16baf7512630eee6eb8f50c9313536eb25672
NuclMe <saltansergei@gmail.com>
2024-07-14 20:35:32 +0300

    completed pagination

--- src/CoinsTable.tsx
+++ src/CoinsTable.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Table, Image, Flex, Typography } from 'antd';
+import ICrypto from './ICrypto';
 const columns = [
   {
     title: 'Name',
@@ -23,26 +24,34 @@ const columns = [
   },
 ];
 export const CoinsTable: React.FC = () => {
-  const [cryptos, setCryptos] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [cryptos, setCryptos] = useState<ICrypto>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<string | null>(null);
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
 
-  const fetchCryptos = async (page, pageSize) => {
-    setIsLoading(true);
-    try {
-      const response = await fetch(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${pageSize}&page=${page}&sparkline=false`
-      );
-      const data = await response.json();
-      setCryptos(data);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  const fetchCryptos = useCallback(
+    async (page: number, pageSize: number) => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${pageSize}&page=${page}&sparkline=false`
+        );
+        const data = await response.json();
+        const dataWithKeys = data.map((item: ICrypto) => ({
+          ...item,
+          key: item.id,
+        }));
+        setCryptos(dataWithKeys);
+      } catch (error) {
+        setIsError(error);
+        alert(`Please try again later, server response is: ${isError}`);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [isError]
+  );
 
   useEffect(() => {
     fetchCryptos(1, 10);
