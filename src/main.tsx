6453e7e6eb8af1d44de221766a1c1e77710b671e
NuclMe <saltansergei@gmail.com>
2024-07-16 14:35:08 +0300

    added codeMirrorEditor

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import CodeMirrorEditor from './CodeMirrorEditor';
+import { Flex } from 'antd';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Flex gap="middle" vertical>
+      <App />
+      <CodeMirrorEditor />
+    </Flex>
   </React.StrictMode>
 );
